a57d6012654fde41fb3d67d6a5d68f24c2783148
plentysmart <github@plentysmart.co.uk>
2014-07-19 21:00:26 +0100

    MaxDegreeOfParallelism sample

--- DataflowPlayground/DataflowPlayground/Blocks/ActionBlock.cs
+++ DataflowPlayground/DataflowPlayground/Blocks/ActionBlock.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -39,6 +40,7 @@ namespace DataflowPlayground.Blocks
             Debug.WriteLine("{0} ms - finished", sp.ElapsedMilliseconds);
             Assert.Empty(list);
         }
+
         [Fact]
         public void BasicUsage_Async()
         {
@@ -102,27 +104,57 @@ namespace DataflowPlayground.Blocks
                 MaxMessagesPerTask = 2
             };
             int executions = 0;
-         
             var waitingActcionBlock = new ActionBlock<int>((x) =>
             {
                 /* Normally there would be one task running this block in a loop
-                 * Something like:
+                 * Something like ( System.Threading.Tasks.Dataflow.Internal.TargetCore`1.ProcessMessagesLoopCore()):
                  * while(this.Recieve())
                  * {
                  *  @action(x)
                  * }
                  * MaxMessagesPerTask will limit number of messages processed by one task.
-                 * System.Threading.Tasks.Dataflow.Internal.TargetCore`1.ProcessMessagesLoopCore()
+                 *
                  * Not really sure how to check if it's running in a new task
                  */
                 executions++;
             }, options);
+
             Assert.True( waitingActcionBlock.Post(100));
             // Returns false if capacity excided
             waitingActcionBlock.Post(100);
             waitingActcionBlock.Complete();
             waitingActcionBlock.Completion.Wait();
             Assert.Equal(2, executions);
+        }
+
+        [Fact]
+        public void ExecutionOptions_MaxDegreeOfParallelism()
+        {
+            Assert.True(Environment.ProcessorCount > 1,
+                "You really want to see parallel execution on single processor machine?!?!?!");
+            var concurrencyObserved = false;
+            var options = new ExecutionDataflowBlockOptions()
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+                //MaxDegreeOfParallelism = 1 //uncomment this line to the difference
+            };
+            var enterFlag = false;
+            var concurrentBlock = new ActionBlock<int>((x) =>
+            {
+                enterFlag = true;
+                Thread.Sleep(100);
+                if (!enterFlag)
+                    concurrencyObserved = true;
+                enterFlag = false;
+
+            }, options);
+            for (int i = 0; i < 10; i++)
+            {
+                concurrentBlock.Post(i);
+            }
+            concurrentBlock.Complete();
+            concurrentBlock.Completion.Wait();
+            Assert.True(concurrencyObserved);
         }
     }
 }
