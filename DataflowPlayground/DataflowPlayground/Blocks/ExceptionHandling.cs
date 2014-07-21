eae6cae80d1c7f14b0f2fcab7d914630483342e0
plentysmart <github@plentysmart.co.uk>
2014-07-21 21:14:19 +0100

    Exception Handling first approach

--- DataflowPlayground/DataflowPlayground/Blocks/ExceptionHandling.cs
+++ DataflowPlayground/DataflowPlayground/Blocks/ExceptionHandling.cs
@@ -28,20 +28,20 @@ namespace DataflowPlayground.Blocks
                 processedValues.Add(x);
                 return x/2;
             });
-            Assert.True( divideBlock.Post(2));
-            Assert.True( divideBlock.Post(3));
-            Assert.True( divideBlock.Post(6));
+            Assert.True(divideBlock.Post(2));
+            Assert.True(divideBlock.Post(3));
+            Assert.True(divideBlock.Post(6));
             divideBlock.Complete();
 
             while (await divideBlock.OutputAvailableAsync())
             {
-                Assert.DoesNotThrow(() =>
-                {
+              
                     var value = divideBlock.Receive();
                     Debug.WriteLine(value);
-                });
+               
             }
             Assert.Equal(1, processedValues.Count); // Only first message processed
+            Assert.Throws<AggregateException>(() => divideBlock.Completion.Wait());
             Assert.True(divideBlock.Completion.IsFaulted);
         }
 
@@ -69,13 +69,85 @@ namespace DataflowPlayground.Blocks
             Assert.True(divideBlock.Post(3));
             while (await divideBlock.OutputAvailableAsync())
             {
-                Assert.DoesNotThrow(() =>
-                {
                     var value = divideBlock.Receive();
-                    Debug.WriteLine(value);
-                });
+                    Debug.WriteLine(value);  
             }
             Assert.False(divideBlock.Post(2));
+            Assert.Throws<AggregateException>(() => divideBlock.Completion.Wait());
+        }
+
+        [Fact]
+        public void ExceptionHandlig_FirstApproach()
+        {
+            ExceptionHandlig_FirstApproachSyncWrapper().Wait();
+        }
+
+        private static async Task ExceptionHandlig_FirstApproachSyncWrapper()
+        {
+            var processedValues = new List<int>();
+            Func<int, MessageOut<int, int>> transform = (x) =>
+            {
+                if (x%2 != 0)
+                    return new MessageOut<int, int>(x, new ArgumentException("This block can process only even numbers"));
+                processedValues.Add(x);
+                return new MessageOut<int, int>(x, x/2);
+            };
+
+            var divideBlock = new TransformBlock<int, MessageOut<int, int>>(transform);
+            divideBlock.Post(2);
+            divideBlock.Post(3);
+            divideBlock.Post(4);
+            divideBlock.Post(6);
+            divideBlock.Complete();
+            var sucessfull = 0;
+            var failed = 0;
+            while (await divideBlock.OutputAvailableAsync())
+            {
+                    var value = divideBlock.Receive();
+                    if (value.IsFaulted) failed++;
+                    else
+                    {
+                        sucessfull++;
+                        Debug.WriteLine(value.Output);
+                    }
+            }
+            Assert.DoesNotThrow(() => divideBlock.Completion.Wait());
+
+            Assert.Equal(3, sucessfull);
+            Assert.Equal(1, failed);
+        }
+
+        public class MessageOut<TIn, TOut>
+        {
+            private readonly Exception _exception;
+            private TOut _output;
+            public TIn Input { get; private set; }
+
+            public TOut Output
+            {
+                get
+                {
+                    if (IsFaulted)
+                        throw _exception ?? new ArgumentException("Message is faulted. Detailed exception not available");
+                    return _output;
+                }
+                private set { _output = value; }
+            }
+
+
+            public MessageOut(TIn input,TOut output)
+            {
+                Input = input;
+                Output = output;
+            }
+            public MessageOut(TIn input, Exception exception)
+            {
+                _exception = exception;
+                Input = input;
+                IsFaulted = true;
+            }
+
+            public bool IsFaulted { get; private set; }
         }
     }
 }
